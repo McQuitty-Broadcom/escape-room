b1fa43b094ab8557d048bb13575b4c5f10147178
Thomas McQuitty <you@example.com>
2022-12-05 14:30:14 -0600

    Asd

--- solution.sh
+++ solution.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 alias ret='zowe endevor ret element MARBLE01 --type COBOL --tf mb01.txt'
-alias upd='zowe endevor ret element MARBLE01 --type COBOL --ff mb01.txt'
+alias upd='zowe endevor update element MARBLE01 --type COBOL --ff mb01.txt'
 alias gen='zowe enedevor generate element MARBLE01 --type COBOL && zowe enedevor generate element MARBLE01 --type LINK'
 alias marbcopy='zowe jobs submit ds "cust001.marbles.jcl(marbcopy) --wfo'
 alias marbind='zowe jobs submit ds "cust001.marbles.jcl(marbind) --wfo'
