1d67c03e7ac727e6988993b454a4036ebd7f7605
Thomas McQuitty <you@example.com>
2022-12-05 14:33:48 -0600

    Asd

--- solution.sh
+++ solution.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 alias ret='zowe endevor ret element MARBLE01 --type COBOL --tf mb01.txt'
 alias upd='zowe endevor update element MARBLE01 --type COBOL --ff mb01.txt'
-alias gen='zowe enedevor generate element MARBLE01 --type COBOL && zowe enedevor generate element MARBLE01 --type LINK'
-alias marbcopy='zowe jobs submit ds "cust001.marbles.jcl(marbcopy) --wfo'
-alias marbind='zowe jobs submit ds "cust001.marbles.jcl(marbind) --wfo'
+alias gen='zowe endevor generate element MARBLE01 --type COBOL && zowe endevor generate element MARBLE01 --type LINK'
+alias marbcopy='zowe jobs submit ds "cust001.marbles.jcl(marbcopy)" --wfo'
+alias marbind='zowe jobs submit ds "cust001.marbles.jcl(marbind)" --wfo'
 alias cics='zowe cics refresh program MARBLE01'
 alias run_prog='zowe console issue command "F CICSTRN1,MB01 CRE RED 10 10" --cn cust001'
+alias del_marb='zowe console issue command "F CICSTRN1,MB01 DEL RED" --cn cust001'
